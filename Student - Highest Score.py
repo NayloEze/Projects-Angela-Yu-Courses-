6e61cb28673fdc96fdbf762f04233ae474e77365
NayloEze <91425400+NayloEze@users.noreply.github.com>
2022-06-29 05:59:18 -0400

    Add files via upload
    Highest Score - Write a program to calculate the highest score from al list of student scores using a for loop and without the use of the Max function.

---
+++ Student - Highest Score.py
@@ -0,0 +1,11 @@
+student_score = [150, 142, 185, 120, 171, 184, 149, 199]
+for n in range(0, len(student_score)):
+    print(n)
+
+highest_score = 0
+for score in student_score:
+    if score > highest_score:
+    
+        highest_score = score
+print(highest_score)
+    #150, 142, 115, 120, 171, 184, 149
